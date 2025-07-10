7961c5a4c1512d69b6c4e6d42a53f1c656cd0d0f
Ashmit0 <ashbathla06@gmail.com>
2025-07-10 09:00:01 +0000

    added terminal inputs feature

--- getiv.py
+++ getiv.py
@@ -1,7 +1,18 @@
 import yaml 
+import sys 
 
 with open('user_input.yaml', 'r') as f:
     inputs = yaml.safe_load(f)
+
+if len(sys.argv) >= 3 : 
+    inputs['underlying'] = sys.argv[1]
+    inputs['start_date'] = sys.argv[2] 
+    if len( sys.argv ) == 4 : 
+        inputs['end_date'] = sys.argv[2]
+        inputs['exp'] = sys.argv[3] 
+    else : 
+        inputs['end_date'] = sys.argv[3]
+        inputs['exp'] = sys.argv[4]
 
 from atm_data_lib.base import atm_data 
 
