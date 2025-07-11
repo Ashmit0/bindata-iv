ee9835caef7fed6b54522922fe474f8a8846100f
Ashmit0 <ashbathla06@gmail.com>
2025-07-11 11:30:58 +0000

    updated to exclude dates that are not found

--- atm_data_lib/base.py
+++ atm_data_lib/base.py
@@ -35,19 +35,27 @@ class atm_data:
 
         # list of working dates 
         datesf1 = pd.date_range( start= inputs['start_date'] , end = inputs['end_date'] , freq = 'D' )
-        datesf1 = drop_weekends( datesf1 )
         self.datesf1 = datesf1.strftime( date_format= format1 ).to_list()
+        #datesf1 = drop_weekends( datesf1 )
 
-        # dates in standard format 
-        self.datesf2 = format_dates( self.datesf1)
 
         # lof file paths : 
-        self.dates_log_path = [ get_log_file_path( date ) for date in self.datesf1 ]
-
+        self.dates_log_path = [ get_log_file_path( date ) for date in self.datesf1 if os.path.isfile(get_log_file_path(date)) ]
+        dates_droped  = [ date  for date in self.datesf1 if ( not  os.path.isfile(get_log_file_path(date)) ) ]
+        self.datesf1 = [ date for date in self.datesf1 if ( os.path.isfile(get_log_file_path(date))) ] 
+        
+        for date in dates_droped : 
+            print(f"{date} not found in {inputs['parent_dir']}...") 
+        
+        # dates in standard format 
+        self.datesf2 = format_dates( self.datesf1)
+        
         self.files = [f[:7] for f in os.listdir(inputs['contractp']) if os.path.isfile(os.path.join(inputs['contractp'], f))]
     
     
     def load_data(self)-> None : 
+        if not len(self.datesf1) : 
+            return 
         # load st gap 
         self.st_gap = get_strike_gap( self.datesf2 , self.dates_log_path , self.inputs )
         # load main df : 
@@ -55,7 +63,9 @@ class atm_data:
 
         get_ivs( self.main_df , self.inputs['r'])
 
-    def save( self ) : 
+    def save( self ) :
+        if not len(self.datesf1) : 
+            return 
         self.main_df.to_csv( os.path.join(self.inputs['outfile'] , '_'.join([self.inputs['underlying'],self.inputs['start_date'],self.inputs['end_date'] , self.inputs['exp'] , 'atm_iv.csv'])))
 
 
